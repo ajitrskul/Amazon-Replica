676ee44fe44922d4bd7a0bcc6dfda7b8c1d65f6c
Aurawra <aurawra@outlook.com>
2024-08-12 00:06:10 -0500

    Make quantity selector interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -25,7 +25,7 @@ products.forEach((product) => {
       </div>
 
       <div class="product-quantity-container">
-        <select>
+        <select class="js-quantity-selector-${product.id}">
           <option selected value="1">1</option>
           <option value="2">2</option>
           <option value="3">3</option>
@@ -59,6 +59,7 @@ document.querySelectorAll('.js-add-to-cart')
   .forEach((button) => {
     button.addEventListener('click', () => {
       const productId = button.dataset.productId;
+      const quantitySelected = document.querySelector(`.js-quantity-selector-${productId}`).value;
 
       let matchingItem;
 
@@ -69,12 +70,12 @@ document.querySelectorAll('.js-add-to-cart')
       });
 
       if (matchingItem) {
-        matchingItem.quantity += 1;
+        matchingItem.quantity += Number(quantitySelected);
       }
       else {
         cart.push({
           productId: productId,
-          quantity: 1
+          quantity: Number(quantitySelected)
         });
       }
 
